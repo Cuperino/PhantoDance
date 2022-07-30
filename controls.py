4de0d3c1c057b88a5b7cae286f6e3ceca4bfb7ba
Javier O. Cordero Pérez <javiercorderoperez@gmail.com>
2022-07-30 18:58:28 -0400

    Got mediapipe working inside Godot.

--- controls.py
+++ controls.py
@@ -1,9 +1,11 @@
 from godot import exposed, export
 from godot import *
+import cv2
+import mediapipe as mp
 
 
 @exposed
-class controls(Node):
+class main(Node):
 
 	# member variables here, example:
 	a = export(int)
@@ -14,4 +16,41 @@ class controls(Node):
 		Called every time the node is added to the scene.
 		Initialization here.
 		"""
+		print("ready")
 		pass
+
+	def _initialize(self):
+		print("initialized")
+		self.cap = cv2.VideoCapture(0)
+
+	def _iteration(self):
+		print("i")
+		with mp_pose.Pose(
+			min_detection_confidence=0.5,
+			min_tracking_confidence=0.5) as pose:
+			if cap.isOpened():
+				success, image = self.cap.read()
+				if not success:
+					print("Ignoring empty camera frame.")
+					# If loading a video, use 'break' instead of 'continue'.
+					#continue
+
+				# To improve performance, optionally mark the image as not writeable to
+				# pass by reference.
+				image.flags.writeable = False
+				image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+				results = pose.process(image)
+
+				# Draw the pose annotation on the image.
+				image.flags.writeable = True
+				image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+				mp_drawing.draw_landmarks(
+					image,
+					results.pose_landmarks,
+					mp_pose.POSE_CONNECTIONS,
+					landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
+				# Flip the image horizontally for a selfie-view display.
+				cv2.imshow('MediaPipe Pose', cv2.flip(image, 1))
+				if cv2.waitKey(5) & 0xFF == 27:
+					break
+		cap.release()
